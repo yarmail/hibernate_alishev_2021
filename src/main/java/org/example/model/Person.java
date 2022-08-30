5823e5908203ed0c812041263c158d6304508d81
yarmail <yarmail@yandex.ru>
2022-08-30 15:53:37 +0300

    lesson 51 + fix

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -48,6 +48,14 @@ public class Person {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
 
 /**@Entity - сущность
