f81820d8afe74022c4c96371b28499e9cf5facf2
yarmail <yarmail@yandex.ru>
2022-10-04 23:12:33 +0300

    lesson 56 + fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,67 +1,81 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-// 55 Отношение one-to-one в Hibernate
-/*
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//56 Отношение many-to-many в Hibernate
+
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
-
-        try {
+        try (sessionFactory){
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
             /*
-            Создаем человека и добавляем ему паспорт
+            Привяжем актеров к фильмам
              */
             /*
-            Person person = new Person("Test person", 50);
-            Passport passport = new Passport(12345);
-            person.setPassport(passport);
-            session.save(person);
+            Movie movie = new Movie("Pulp fiction", 1994);
+            Actor actor1 = new Actor("Harvey Keitel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            session.save(movie);
+            session.save(actor1);
+            session.save(actor2);
             */
-            /* -----------------------------------
-            Прочитаем человека и получим его паспорт
-            Прочитаем паспорт и получим человека
-             */
-            //Person person = session.get(Person.class, 2);
-            //System.out.println(person.getPassport().getPassportNumber());
-            /*
-            Passport passport = session.get(Passport.class, 2);
-            System.out.println(passport.getPerson().getName());
-            */
-            /* ------------------------
-            Изменим номер паспорта у какого-то человека
+            /*----------------------
+            Выведем список актеров фильма
+            Потом попробовать вывести список фильмов актера
              */
             /*
-            Person person = session.get(Person.class, 2);
-            person.getPassport().setPassportNumber(77777);
+            Movie movie = session.get(Movie.class, 1);
+            System.out.println(movie.getActors());
             */
-            /* -------------------
-            Удаление человека
+            /* -----------------------------
+            Добавить фильм для существующего актера
              */
-            Person person = session.get(Person.class, 2);
-            session.remove(person);
+            /*
+            Movie movie = new Movie("Reservoir Dogs", 1992);
+            Actor actor = session.get(Actor.class, 1);
+            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
+            actor.getMovies().add(movie);
+            session.save(movie);
+             */
+            /* --------------
+            Удалить фильм у актера
+             */
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+            Movie movieToRemove = actor.getMovies().get(0);
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
+
         }
     }
 }
 // Примечания
 /*
-После модернизации класса Person,
-добавлении "двусторонней связи" в метод
-setPassport() мы
+Вместо try - finally будем использовать
+try with resources (try с ресурсами)
 
+Arrays.asList - изменяемый список меняем
+на List.of - неизменяемый
+
+Каскадирование мы не настраивали поэтому сохраняем
+и 1 и 2 объекты
  */
